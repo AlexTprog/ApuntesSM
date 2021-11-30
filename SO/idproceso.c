5f5dd028cc6dfba741a074a6acd61af8e6cb6499
AlexTprog <alexander.t1780@gmail.com>
2021-11-30 14:37:38 -0500

    deshaciendo respaldo

--- SO/idproceso.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-int main(void){
-	printf("Id proceso hijo = %d \n", getpid());
-	printf("Id proceso padre = %d \n", getppid());
-	return 0;
-}
