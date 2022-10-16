27688a5844da266a899a930c6e7b937982addece
alextprog <alexander.t1780@gmail.com>
2022-10-15 21:53:04 -0500

    s5

--- CICLO-VIII/Programacion Paralela/Parte_practica/semana4/busqueda_mejorada.cpp
+++ CICLO-VIII/Programacion Paralela/Parte_practica/semana4/busqueda_mejorada.cpp
@@ -14,11 +14,12 @@ int main() {
   for (int i = 0; i < n; i++) {
     int temp;
     cin >> temp;
-    A[i] = temp;
+    A.push_back(temp);
   }
 
   int NUM_THREADS = 2;
   omp_set_num_threads(NUM_THREADS);
+  auto start = std::steady_clock::now()
   int found = -1;
 #pragma omp parallel
   {
@@ -27,8 +28,13 @@ int main() {
       if (A[i] == x) {
         found = i;
       }
+      if(found>=0){
+        break;
+      }
     }
   }
+auto end = std::chrono::steady_clock::now();
+std::chrono::stedy_clock::now()
 
   if (found >= 0) {
     cout << "encontrado " << found << endl;
---
+++ CICLO-VIII/Seguridad de SW/S4 - Vulnerabilidades.md
@@ -0,0 +1,9 @@
+# Vulnerabilidades
+
+### Top 10 Vulnerabilities 2021
+- Broken Acess Control
+Permite que un usuario sin privilegios acceda a un recurso. 
+- Cryptographic Failures
+- Injection
+- Insecure Desing
+- Security
